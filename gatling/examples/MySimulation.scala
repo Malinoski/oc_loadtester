986b52e8e018f16ae83ea54ee3d50333f60fac2d
malinoski <malinoski.iuri@gmail.com>
2015-08-03 15:10:51 -0300

    code improvements

--- gatling/examples/MySimulation.scala
+++ gatling/examples/MySimulation.scala
@@ -1,4 +1,4 @@
-//Command line example: JAVA_OPTS="-DbaseUrl=http://localhost" $GATLING_HOME/bin/gatling.sh -s MySimulation > out.txt && vim out.txt
+//Command line example: $GATLING_HOME/bin/gatling.sh -s MySimulation > out.txt && vim out.txt
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
@@ -7,20 +7,22 @@ import io.gatling.http.config.HttpProtocolBuilder.toHttpProtocol
 class MySimulation extends Simulation {
 
   //Parameters
-  val nbUsers = 10
+  val nbUsers = 5
   val myRamp: java.lang.Long = 5L
-  val simulationName = "My Simulation 03"
-  val baseUrl = System.getProperty("baseUrl") 
-  val serverName = "owncloud-8.0.3" //owncloud 8
-  //val serverName = "owncloudv2" // owncloud 7
-  val user = "iuri"
-  val password = "iuri"
+  val simulationName = "My Simulation"
+  val baseUrl = "http://localhost" 
+  val serverName = "owncloud-8.1.0"
+  //val serverName = "owncloud-8.0.3"
+  //val serverName = "owncloud-7.0.4"
+  //val serverName = "owncloud-5.0.18"
+  val user = "testuser"
+  val password = "testuser"
 
-  //My Simulation 03
+  //My Simulation (note: the "Documents" folder must exist)
   var builder: TScenarioBuilder = new OCScenarioBuilder(simulationName, baseUrl, serverName, user, password)
   builder.addUploadFile("${userId}.txt", "/")
-  builder.addMoveFile("${userId}.txt", "/", "/documents")
-  builder.addDropFile("${userId}.txt", "/documents")
+  builder.addMoveFile("${userId}.txt", "/", "/Documents")
+  builder.addDropFile("${userId}.txt", "/Documents")
   builder.addCreateFile("${userId}-new", "txt", "/")
   builder.addRenameFile("${userId}-new.txt", "${userId}-renamed.txt", "/")
   builder.addDropFile("${userId}-renamed.txt", "/")
